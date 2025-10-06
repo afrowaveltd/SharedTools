14bc4b7ea48f2c7397f648a75ef9e11bae128c2b
afrowaveltd <65894408+afrowaveltd@users.noreply.github.com>
2025-10-06 23:19:48 +0200

    LanguageService / started to work on

---
+++ Afrowave.SharedTools.LanguageService/Services/LanguageService.cs
@@ -0,0 +1,51 @@
+﻿using Afrowave.SharedTools.Models.Localization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Afrowave.SharedTools.LibreTranslate.Services
+{
+	public class LanguageService
+	{
+		private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+		{
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			DefaultIgnoreCondition = JsonIgnoreCondition.Never,
+			ReferenceHandler = ReferenceHandler.IgnoreCycles
+		};
+
+		private readonly string filePath = Path.Combine(AppContext.BaseDirectory, "Jsons", "languages.json");
+		private readonly List<Language> _languages = new List<Language>();
+
+		public LanguageService()
+		{
+			if(!File.Exists(filePath))
+			{
+				throw new FileNotFoundException($"The file at path {filePath} was not found.");
+			}
+
+			if(File.Exists(filePath))
+			{
+				try
+				{
+					var json = File.ReadAllText(filePath);
+					_languages = JsonSerializer.Deserialize<List<Language>>(json, _options) ?? new List<Language>();
+				}
+				catch(Exception ex)
+				{
+					throw new InvalidOperationException("Failed to load languages from the JSON file.", ex);
+				}
+			}
+		}
+
+		private List<Language> GetLanguages()
+		{
+			var response = new List<Language>(_languages);
+			return response;
+		}
+	}
+}
